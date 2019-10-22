e7a97c4805fc7ff1a2b0f9c645a32e8ea83f7a51
Eleanor Parker <eleanor.parker.19@ucl.ac.uk>
2019-10-22 12:33:57 +0100

    Group of people who know each other

--- group.py
+++ group.py
@@ -1,5 +1,47 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+my_group ={
+	'Jill':{
+		'Age':26
+		'Job':'Biologist'
+		'Relation to others':{
+			'Jill':['self'],
+			'Zalika':['friend'],
+			'John':['partner'],
+			'Nash':[],
+		}
+	}
 
-my_group =
+	'Zalika':{
+		'Age':28
+		'Job':'Artist'
+		'Relation to others':{
+			'Jill':['friend'],
+			'Zalika':['self'],
+			'John':[],
+			'Nash':['tenant'],
+		}
+	}
+
+	'John':{
+		'Age':27
+		'Job':'Writer'
+		'Relation to others':{  
+			'Jill':['partner'],
+                        'Zalika':[],
+                        'John':['self'],
+                        'Nash':['cousin'],
+		}
+	}
+
+	'Nash':{
+		'Age':34
+		'Job':'Writer'
+		'Relation to others':{
+			'Jill':[],
+			'Zalika':['landlord'],
+			'John':['cousin']
+			'Nash':['self']
+		}
+	}
+}
